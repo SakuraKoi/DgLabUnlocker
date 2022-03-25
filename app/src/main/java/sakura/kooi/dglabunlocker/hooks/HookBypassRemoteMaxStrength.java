a05c8b3287580b841b670d496bd9fc6f9fcfde47
SakuraKooi <sakurakoi993519867@gmail.com>
2022-03-25 16:09:16 +0800

    坏.jpg

--- app/src/main/java/sakura/kooi/dglabunlocker/hooks/HookBypassRemoteMaxStrength.java
+++ app/src/main/java/sakura/kooi/dglabunlocker/hooks/HookBypassRemoteMaxStrength.java
@@ -1,27 +1,54 @@
 package sakura.kooi.dglabunlocker.hooks;
 
+import static sakura.kooi.dglabunlocker.GlobalVariables.isRemote;
+import static sakura.kooi.dglabunlocker.GlobalVariables.localStrengthA;
+import static sakura.kooi.dglabunlocker.GlobalVariables.localStrengthB;
+import static sakura.kooi.dglabunlocker.GlobalVariables.maxStrengthA;
+import static sakura.kooi.dglabunlocker.GlobalVariables.maxStrengthB;
+import static sakura.kooi.dglabunlocker.GlobalVariables.totalStrengthA;
+import static sakura.kooi.dglabunlocker.GlobalVariables.totalStrengthB;
+
 import android.content.Context;
+import android.widget.Toast;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import sakura.kooi.dglabunlocker.injector.InjectStrengthAddButton;
 
 public class HookBypassRemoteMaxStrength implements InjectStrengthAddButton.StrengthAddHandler {
+    private final AtomicInteger realMaxA = new AtomicInteger();
+    private final AtomicInteger realMaxB = new AtomicInteger();
     @Override
-    public void beforeStrengthA(Context context) {
-
+    public void beforeStrengthA(Context context) throws IllegalAccessException { // totalStrengthA >= localStrengthA + maxStrengthA
+        if (isRemote.getBoolean(null)) {
+            realMaxA.set(maxStrengthA.getInt(null));
+            maxStrengthA.setInt(null, 276);
+        }
     }
 
     @Override
-    public void beforeStrengthB(Context context) {
-
+    public void beforeStrengthB(Context context) throws IllegalAccessException { // totalStrengthB >= localStrengthB + maxStrengthB
+        if (isRemote.getBoolean(null)) {
+            realMaxB.set(maxStrengthB.getInt(null));
+            maxStrengthB.setInt(null, 276);
+        }
     }
 
     @Override
-    public void afterStrengthA(Context context) {
-
+    public void afterStrengthA(Context context) throws IllegalAccessException {
+        if (isRemote.getBoolean(null)) {
+            if (totalStrengthA.getInt(null) >= localStrengthA.getInt(null) + realMaxA.get())
+                Toast.makeText(context, "绕过远程最大强度 A: " + realMaxA.get(), Toast.LENGTH_SHORT).show();
+            maxStrengthA.setInt(null, realMaxA.get());
+        }
     }
 
     @Override
-    public void afterStrengthB(Context context) {
-
+    public void afterStrengthB(Context context) throws IllegalAccessException {
+        if (isRemote.getBoolean(null)) {
+            if (totalStrengthB.getInt(null) >= localStrengthB.getInt(null) + realMaxB.get())
+                Toast.makeText(context, "绕过远程最大强度 B: " + realMaxB.get(), Toast.LENGTH_SHORT).show();
+            maxStrengthB.setInt(null, realMaxB.get());
+        }
     }
 }
