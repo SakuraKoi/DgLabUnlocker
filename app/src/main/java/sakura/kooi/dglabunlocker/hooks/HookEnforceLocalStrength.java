f8e07af22bb7cb5870519fc0de0ba17a569e0daf
SakuraKooi <sakurakoi993519867@gmail.com>
2022-04-13 17:36:05 +0800

    refactor: 更改注入点, 优化性能

--- app/src/main/java/sakura/kooi/dglabunlocker/hooks/HookEnforceLocalStrength.java
+++ app/src/main/java/sakura/kooi/dglabunlocker/hooks/HookEnforceLocalStrength.java
@@ -1,31 +1,23 @@
 package sakura.kooi.dglabunlocker.hooks;
 
-import static sakura.kooi.dglabunlocker.GlobalVariables.classControlledTouchListeners;
-
 import android.content.Context;
 
-import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import sakura.kooi.dglabunlocker.GlobalVariables;
 
 public class HookEnforceLocalStrength {
     public static final HookEnforceLocalStrength INSTANCE = new HookEnforceLocalStrength();
-    private HookEnforceLocalStrength() {}
+
+    private HookEnforceLocalStrength() {
+    }
 
     private AtomicInteger localStrengthA = new AtomicInteger();
     private AtomicInteger localStrengthB = new AtomicInteger();
 
-    public void handleLocalStrengthChange(Context context, AtomicInteger localStrengthA, AtomicInteger localStrengthB) {
-        if (Arrays.stream(Thread.currentThread().getStackTrace())
-                .map(StackTraceElement::getClassName)
-                .anyMatch(clazz -> classControlledTouchListeners.contains(clazz))) {
-            this.localStrengthA.set(localStrengthA.get());
-            this.localStrengthB.set(localStrengthB.get());
-        } else {
-            localStrengthA.set(this.localStrengthA.get());
-            localStrengthB.set(this.localStrengthB.get());
-        }
+    public void handleLocalStrengthChange(Context context) throws IllegalAccessException {
+        this.localStrengthA.set(GlobalVariables.localStrengthA.getInt(null));
+        this.localStrengthB.set(GlobalVariables.localStrengthB.getInt(null));
     }
 
     public void afterStrengthDecode(Context context) throws IllegalAccessException {
--- app/src/main/java/sakura/kooi/dglabunlocker/injector/InjectControlledStrengthButton.java
+++ app/src/main/java/sakura/kooi/dglabunlocker/injector/InjectControlledStrengthButton.java
@@ -3,8 +3,10 @@ package sakura.kooi.dglabunlocker.injector;
 import static sakura.kooi.dglabunlocker.utils.ExceptionLogger.withCatch;
 
 import android.content.Context;
+import android.view.MotionEvent;
+import android.view.View;
 
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Arrays;
 
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
@@ -14,19 +16,18 @@ import sakura.kooi.dglabunlocker.hooks.HookEnforceLocalStrength;
 public class InjectControlledStrengthButton implements IHookPointInjector {
 
     public void apply(Context context, ClassLoader classLoader) {
-            XposedHelpers.findAndHookMethod("com.bjsm.dungeonlab.service.RemotelyService2", classLoader,
-                    "a", int.class, int.class, new XC_MethodHook() {
+
+        Arrays.stream(GlobalVariables.classTouchListeners).forEach(classTouchListener -> {
+            XposedHelpers.findAndHookMethod(classTouchListener, classLoader,
+                    "onTouch", View.class, MotionEvent.class, new XC_MethodHook() {
                         @Override
-                        protected void beforeHookedMethod(MethodHookParam param) {
-                            AtomicInteger localStrengthA = new AtomicInteger((int)param.args[0]);
-                            AtomicInteger localStrengthB = new AtomicInteger((int)param.args[1]);
+                        protected void afterHookedMethod(MethodHookParam param) {
                             withCatch("HookEnforceLocalStrength", () -> {
                                 if (GlobalVariables.enforceLocalStrength)
-                                    HookEnforceLocalStrength.INSTANCE.handleLocalStrengthChange(context, localStrengthA, localStrengthB);
+                                    HookEnforceLocalStrength.INSTANCE.handleLocalStrengthChange(context);
                             });
-                            param.args[0] = localStrengthA.get();
-                            param.args[1] = localStrengthB.get();
                         }
                     });
+        });
     }
 }
