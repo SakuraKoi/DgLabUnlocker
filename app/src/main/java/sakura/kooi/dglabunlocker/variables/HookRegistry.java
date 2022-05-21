d0d0ee43e0e7629e51993d13508a3e9c08d7f59f
SakuraKooi <root@sakurakooi.cyou>
2022-05-21 14:35:45 +0800

    remove: delete DeviceProtection ( close #16 )

--- app/src/main/java/sakura/kooi/dglabunlocker/variables/HookRegistry.java
+++ app/src/main/java/sakura/kooi/dglabunlocker/variables/HookRegistry.java
@@ -9,7 +9,6 @@ import java.util.function.Supplier;
 
 import sakura.kooi.dglabunlocker.features.AbstractFeature;
 import sakura.kooi.dglabunlocker.features.FeatureBypassRemoteMaxStrength;
-import sakura.kooi.dglabunlocker.features.FeatureDeviceProtection;
 import sakura.kooi.dglabunlocker.features.FeatureEnforceLocalStrength;
 import sakura.kooi.dglabunlocker.features.FeatureEnforceRemoteMaxStrength;
 import sakura.kooi.dglabunlocker.features.FeatureUnlockRemoteMax;
@@ -21,7 +20,6 @@ public class HookRegistry {
     public static final boolean ENABLE_DEV_FEATURE = false;
 
     public static final List<Class<? extends AbstractFeature>> features = Collections.unmodifiableList(Arrays.asList(
-            FeatureDeviceProtection.class,
             FeatureUnlockRemoteMax.class,
             FeatureEnforceLocalStrength.class,
             FeatureEnforceRemoteMaxStrength.class,
