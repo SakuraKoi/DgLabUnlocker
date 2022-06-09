ce9f844f203384f9e78b03725d09940f54298738
SakuraKooi <root@sakurakooi.cyou>
2022-06-09 17:46:35 +0800

    cleanup: remove unused classes

--- app/src/main/java/sakura/kooi/dglabunlocker/hooks/IHook.java
+++
@@ -1,7 +0,0 @@
-package sakura.kooi.dglabunlocker.hooks;
-
-import android.content.Context;
-
-public interface IHook {
-    void apply(Context context, ClassLoader classLoader) throws ReflectiveOperationException;
-}
