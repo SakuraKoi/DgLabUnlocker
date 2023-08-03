a6ad86a6f2200a9230653d8f63d1eb609563cb22
SakuraKooi <root@sakurakooi.cyou>
2023-08-03 23:03:00 +0800

    refactor: target DG-Lab Beta

---
+++ app/src/main/java/dev/sakurakooi/dglabunlocker/XposedModuleInit.java
@@ -0,0 +1,24 @@
+package dev.sakurakooi.dglabunlocker;
+
+import de.robv.android.xposed.IXposedHookInitPackageResources;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.IXposedHookZygoteInit;
+import de.robv.android.xposed.callbacks.XC_InitPackageResources;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class XposedModuleInit implements IXposedHookLoadPackage, IXposedHookZygoteInit, IXposedHookInitPackageResources {
+    @Override
+    public void handleInitPackageResources(XC_InitPackageResources.InitPackageResourcesParam resparam) throws Throwable {
+
+    }
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) throws Throwable {
+
+    }
+
+    @Override
+    public void initZygote(StartupParam startupParam) throws Throwable {
+
+    }
+}
